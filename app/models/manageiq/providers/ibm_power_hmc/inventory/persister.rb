e19a050cdc91d38f446857d50c57d9ae945b8112
Damien Bergamini <dbergami@us.ibm.com>
2021-09-16 15:42:56 +0200

    start adding some host and vm hardware

--- app/models/manageiq/providers/ibm_power_hmc/inventory/persister.rb
+++ app/models/manageiq/providers/ibm_power_hmc/inventory/persister.rb
@@ -4,5 +4,7 @@ class ManageIQ::Providers::IbmPowerHmc::Inventory::Persister < ManageIQ::Provide
   def initialize_inventory_collections
     add_collection(infra, :hosts)
     add_collection(infra, :vms)
+    add_collection(infra, :host_hardwares)
+    add_collection(infra, :hardwares)
   end
 end
