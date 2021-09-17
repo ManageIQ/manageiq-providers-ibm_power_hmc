9212d04a5ecf4af23ee6cbef7b0a552c3d173a7b
Damien Bergamini <dbergami@us.ibm.com>
2021-09-17 08:42:36 +0200

    Move persister code into persister directory.

--- app/models/manageiq/providers/ibm_power_hmc/inventory/persister.rb
+++ app/models/manageiq/providers/ibm_power_hmc/inventory/persister.rb
@@ -1,10 +1,3 @@
 class ManageIQ::Providers::IbmPowerHmc::Inventory::Persister < ManageIQ::Providers::Inventory::Persister
   require_nested :InfraManager
-
-  def initialize_inventory_collections
-    add_collection(infra, :hosts)
-    add_collection(infra, :vms)
-    add_collection(infra, :host_hardwares)
-    add_collection(infra, :hardwares)
-  end
 end
--- app/models/manageiq/providers/ibm_power_hmc/inventory/persister/infra_manager.rb
+++ app/models/manageiq/providers/ibm_power_hmc/inventory/persister/infra_manager.rb
@@ -1,2 +1,8 @@
 class ManageIQ::Providers::IbmPowerHmc::Inventory::Persister::InfraManager < ManageIQ::Providers::IbmPowerHmc::Inventory::Persister
+  def initialize_inventory_collections
+    add_collection(infra, :hosts)
+    add_collection(infra, :vms)
+    add_collection(infra, :host_hardwares)
+    add_collection(infra, :hardwares)
+  end
 end
