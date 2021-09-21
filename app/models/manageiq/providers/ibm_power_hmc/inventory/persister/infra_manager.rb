c664c8542e016d875d56b03ae5c96b7a7e89b073
Damien Bergamini <dbergami@us.ibm.com>
2021-09-21 09:24:58 +0200

    hardwares need miq_templates (vm_or_template)

--- app/models/manageiq/providers/ibm_power_hmc/inventory/persister/infra_manager.rb
+++ app/models/manageiq/providers/ibm_power_hmc/inventory/persister/infra_manager.rb
@@ -4,5 +4,6 @@ class ManageIQ::Providers::IbmPowerHmc::Inventory::Persister::InfraManager < Man
     add_collection(infra, :vms)
     add_collection(infra, :host_hardwares)
     add_collection(infra, :hardwares)
+    add_collection(infra, :miq_templates) # required by hardwares.vm_or_template
   end
 end
