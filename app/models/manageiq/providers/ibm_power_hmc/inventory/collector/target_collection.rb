22b4e956524e0819c167387ac05acca977337a25
Damien Bergamini <dbergami@us.ibm.com>
2021-09-21 09:23:21 +0200

    make some methods private

--- app/models/manageiq/providers/ibm_power_hmc/inventory/collector/target_collection.rb
+++ app/models/manageiq/providers/ibm_power_hmc/inventory/collector/target_collection.rb
@@ -1,9 +1,37 @@
-class ManageIQ::Providers::IbmPowerHmc::Inventory::Collector::TargetCollection < ManageIQ::Providers::IbmPowerHmc::Inventory::Collector
+class ManageIQ::Providers::IbmPowerHmc::Inventory::Collector::TargetCollection < ManageIQ::Providers::IbmPowerHmc::Inventory::Collector::InfraManager
   def initialize(_manager, _target)
     super
 
     parse_targets!
   end
+
+  def collect!
+    $ibm_power_hmc_log.info("#{self.class}##{__method__}")
+  end
+
+  def hosts
+    $ibm_power_hmc_log.info("#{self.class}##{__method__}")
+
+    @hosts ||= begin
+      references(:hosts).map do |ems_ref|
+        $ibm_power_hmc_log.info("#{self.class}##{__method__} #{ems_ref}")
+        connection.managed_system(ems_ref)
+      end
+    end
+  end
+
+  def vms
+    $ibm_power_hmc_log.info("#{self.class}##{__method__}")
+
+    @vms ||= begin
+      references(:vms).map do |ems_ref|
+        $ibm_power_hmc_log.info("#{self.class}##{__method__} #{ems_ref}")
+        connection.lpar(ems_ref)
+      end
+    end
+  end
+
+  private
 
   def parse_targets!
     $ibm_power_hmc_log.info("#{self.class}##{__method__}")
@@ -26,23 +54,5 @@ class ManageIQ::Providers::IbmPowerHmc::Inventory::Collector::TargetCollection <
 
   def references(collection)
     target.manager_refs_by_association&.dig(collection, :ems_ref)&.to_a&.compact || []
-  end
-
-  def hosts
-    @hosts ||= begin
-      references(:hosts).map do |ems_ref|
-        $ibm_power_hmc_log.info("#{self.class}##{__method__} #{ems_ref}")
-        # connection.managed_system(ems_ref)
-      end
-    end
-  end
-
-  def vms
-    @vms ||= begin
-      references(:vms).map do |ems_ref|
-        $ibm_power_hmc_log.info("#{self.class}##{__method__} #{ems_ref}")
-        # connection.lpar(ems_ref)
-      end
-    end
   end
 end
