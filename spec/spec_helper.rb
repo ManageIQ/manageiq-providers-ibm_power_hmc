f6cc0512fb135d8ff967085b3eb4ebe5160f74de
Jay Carman <jwcarman@us.ibm.com>
2021-09-21 09:20:36 -0500

    Configure spec VCR and Rails secret paths

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,21 @@ if ENV['CI']
   SimpleCov.start
 end
 
-Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
-Dir[File.join(__dir__, "support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/shared/**/*.rb")].sort.each { |f| require f }
+Dir[File.join(__dir__, "support/**/*.rb")].sort.each { |f| require f }
 
 require "manageiq-providers-ibm_power_hmc"
+
+VCR.configure do |config|
+  # Configure VCR to use rspec metadata.
+  config.hook_into(:webmock)
+  config.configure_rspec_metadata!
+
+  config.ignore_hosts('codeclimate.com') if ENV['CI']
+  config.cassette_library_dir = File.join(ManageIQ::Providers::IbmPowerHmc::Engine.root, 'spec/vcr_cassettes')
+
+  secrets = Rails.application.secrets
+  secrets.ibm_power_hmc.each_key do |secret|
+    config.define_cassette_placeholder(secrets.ibm_power_hmc_defaults[secret]) { secrets.ibm_power_hmc[secret] }
+  end
+end
