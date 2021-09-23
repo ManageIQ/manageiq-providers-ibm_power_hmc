98423cae473e827eea58ec97b9e187d63898cfd4
Damien Bergamini <dbergami@us.ibm.com>
2021-09-23 12:29:33 +0200

    set :host_ems_ref and :vm_ems_ref in event_hash when available

--- app/models/manageiq/providers/ibm_power_hmc/infra_manager/event_parser.rb
+++ app/models/manageiq/providers/ibm_power_hmc/infra_manager/event_parser.rb
@@ -11,6 +11,24 @@ module ManageIQ::Providers::IbmPowerHmc::InfraManager::EventParser
       :full_data  => {:data => event.data, :detail => event.detail},
       :ems_id     => ems_id
     }
+
+    case event.type
+    when /.*_URI/
+      uri = URI(event.data)
+      elems = uri.path.split('/')
+      type, uuid = elems[-2], elems[-1]
+      case type
+      when "ManagedSystem"
+        event_hash[:host_ems_ref] = uuid
+      when "LogicalPartition", "VirtualIOServer"
+        event_hash[:vm_ems_ref] = uuid
+        # Check if the URI also contains /ManagedSystem/{uuid}/
+        if elems.length >= 4 && elems[-4] == "ManagedSystem"
+          event_hash[:host_ems_ref] = elems[-3]
+        end
+      end
+    end
+
     event_hash
   end
 end
