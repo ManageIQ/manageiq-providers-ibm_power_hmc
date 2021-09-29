bc9e6eb84d98e6ae93ea5319d1984240d021cbbe
Damien Bergamini <dbergami@us.ibm.com>
2021-09-29 09:46:24 +0200

    fix handling of DELETE_URI events

--- app/models/manageiq/providers/ibm_power_hmc/infra_manager/event_target_parser.rb
+++ app/models/manageiq/providers/ibm_power_hmc/infra_manager/event_target_parser.rb
@@ -16,7 +16,7 @@ class ManageIQ::Providers::IbmPowerHmc::InfraManager::EventTargetParser
     raw_event = ems_event.full_data
 
     case ems_event.event_type
-    when "MODIFY_URI", "ADD_URI" # Damien: DELETE_URI, INVALID_URI?
+    when "MODIFY_URI", "ADD_URI", "DELETE_URI" # Damien: INVALID_URI?
       uri = URI(raw_event[:data])
       elems = uri.path.split('/')
       type, uuid = elems[-2], elems[-1]
@@ -24,13 +24,14 @@ class ManageIQ::Providers::IbmPowerHmc::InfraManager::EventTargetParser
       when "ManagedSystem"
         $ibm_power_hmc_log.info("#{self.class}##{__method__} managed system uuid #{uuid}")
         target_collection.add_target(:association => :hosts, :manager_ref => {:ems_ref => uuid})
-      when "LogicalPartition" #, "VirtualIOServer"
+      when "LogicalPartition" # Damien: "VirtualIOServer"
         # raw_event[:detail] contains information about the properties that
         # have changed (e.g. RMCState, PartitionName, PartitionState etc...)
         # This may be used to perform quick property REST API calls to the HMC
         # instead of querying the full LPAR data.
         $ibm_power_hmc_log.info("#{self.class}##{__method__} LPAR uuid #{uuid}")
         target_collection.add_target(:association => :vms, :manager_ref => {:ems_ref => uuid})
+      # Damien: when "VirtualSwitch"
       end
     end
 
