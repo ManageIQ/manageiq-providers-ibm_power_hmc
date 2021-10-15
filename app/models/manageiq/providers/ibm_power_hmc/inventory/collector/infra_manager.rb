e2f9a3c5c69c0c31c6ab4cfd23d3234597afe833
Damien Bergamini <dbergami@us.ibm.com>
2021-10-15 15:49:38 +0200

    retrieve cpu_speed for hosts

--- app/models/manageiq/providers/ibm_power_hmc/inventory/collector/infra_manager.rb
+++ app/models/manageiq/providers/ibm_power_hmc/inventory/collector/infra_manager.rb
@@ -6,6 +6,8 @@ class ManageIQ::Providers::IbmPowerHmc::Inventory::Collector::InfraManager < Man
   def collect!
     $ibm_power_hmc_log.info("#{self.class}##{__method__}")
     manager.with_provider_connection do |connection|
+      @hmc = connection.management_console
+
       @cecs = connection.managed_systems
 
       @lpars = @cecs.map do |sys|
@@ -21,6 +23,14 @@ class ManageIQ::Providers::IbmPowerHmc::Inventory::Collector::InfraManager < Man
         $ibm_power_hmc_log.error("vioses query failed for #{sys.uuid} #{e}")
         nil
       end.flatten.compact
+
+      @cpu_freqs = {}
+      @cecs.each do |sys|
+        freq = cpu_freq(connection, sys)
+        @cpu_freqs[sys.uuid] = freq unless freq.nil?
+      rescue => e
+        $ibm_power_hmc_log.error("cpu freq query failed for #{sys.uuid}: #{e}")
+      end
 
       $ibm_power_hmc_log.info("end collection")
     rescue IbmPowerHmc::Connection::HttpError => e
@@ -38,5 +48,25 @@ class ManageIQ::Providers::IbmPowerHmc::Inventory::Collector::InfraManager < Man
 
   def vioses
     @vioses || []
+  end
+
+  def cpu_freqs
+    @cpu_freqs || {}
+  end
+
+  def cpu_freq(connection, sys)
+    # Retrieve the CPU frequency from one of the VIOSes with RMC active.
+    vios = vioses.select { |vios| vios.sys_uuid == sys.uuid && vios.rmc_state == "active" }.first
+    return if vios.nil?
+
+    cmd = "lsdev -dev proc0 -attr frequency"
+    job = connection.cli_run(@hmc.uuid, "viosvrcmd -m \"#{sys.name}\" -p \"#{vios.name}\" -c \"#{cmd}\"")
+    ret = job.results["returnCode"]&.to_i
+    return if ret != 0
+
+    result = job.results["result"]
+    return if result.nil?
+
+    result.split("\n").last.to_f / 1000000.0
   end
 end
