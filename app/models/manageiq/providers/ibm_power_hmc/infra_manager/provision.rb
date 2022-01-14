374afa71f3fcbf6625372f6e613689c620b124dc
Vincent Febvre <vfebvre@us.ibm.com>
2022-01-14 18:00:52 +0100

    Validate request type

--- app/models/manageiq/providers/ibm_power_hmc/infra_manager/provision.rb
+++ app/models/manageiq/providers/ibm_power_hmc/infra_manager/provision.rb
@@ -1,7 +1,14 @@
 class ManageIQ::Providers::IbmPowerHmc::InfraManager::Provision < MiqProvision
   include_concern 'Cloning'
   include_concern 'StateMachine'
+
+  VALID_REQUEST_TYPES = %w(clone_to_template)
+  validates_inclusion_of :request_type, :in => VALID_REQUEST_TYPES, :message => "should be one of: #{VALID_REQUEST_TYPES.join(', ')}"
+
   def destination_type
-    "Vm"
+    case request_type
+    when 'clone_to_template'       then "Template"
+    else                                ""
+    end
   end
 end
