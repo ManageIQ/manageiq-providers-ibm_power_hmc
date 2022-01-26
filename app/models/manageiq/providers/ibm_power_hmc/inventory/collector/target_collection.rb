b302344e903e468023bc9f7726792eb9b89dbad7
Vincent Febvre <vfebvre@us.ibm.com>
2022-01-26 15:04:31 +0100

    Fix rebase error

--- app/models/manageiq/providers/ibm_power_hmc/inventory/collector/target_collection.rb
+++ app/models/manageiq/providers/ibm_power_hmc/inventory/collector/target_collection.rb
@@ -125,17 +125,13 @@ class ManageIQ::Providers::IbmPowerHmc::Inventory::Collector::TargetCollection <
       when HostSwitch
         add_target!(:host_virtual_switches, target.ems_ref)
       when Lan
-<<<<<<< HEAD
         add_target!(:lans, target.ems_ref)
       when ManageIQ::Providers::InfraManager::Template
         add_target!(:miq_templates, target.ems_ref)
+      when Storage
+        add_target!(:storages, target.ems_ref)
       else
         $ibm_power_hmc_log.info("#{self.class}##{__method__} WHAT IS THE CLASS NAME ? #{target.class.name} ")
-=======
-        add_target(:lans, target.ems_ref)
-      when Storage
-        add_target(:storages, target.ems_ref)
->>>>>>> 28ba916 (Targeted refresh)
       end
     end
   end
