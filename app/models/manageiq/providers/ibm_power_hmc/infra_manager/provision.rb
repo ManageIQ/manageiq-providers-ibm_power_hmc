91da2a327640b87624cb2b5bd7f8cc355d3ffa1c
Vincent Febvre <vfebvre@us.ibm.com>
2022-02-08 12:57:07 +0100

    Fix rubocop offences

--- app/models/manageiq/providers/ibm_power_hmc/infra_manager/provision.rb
+++ app/models/manageiq/providers/ibm_power_hmc/infra_manager/provision.rb
@@ -2,13 +2,13 @@ class ManageIQ::Providers::IbmPowerHmc::InfraManager::Provision < MiqProvision
   include_concern 'Cloning'
   include_concern 'StateMachine'
 
-  VALID_REQUEST_TYPES = %w(clone_to_template)
-  validates_inclusion_of :request_type, :in => VALID_REQUEST_TYPES, :message => "should be one of: #{VALID_REQUEST_TYPES.join(', ')}"
+  VALID_REQUEST_TYPES = %w[clone_to_template].freeze
+  validates :request_type, :inclusion => {:in => VALID_REQUEST_TYPES, :message => "should be one of: #{VALID_REQUEST_TYPES.join(', ')}"}
 
   def destination_type
     case request_type
-    when 'clone_to_template'       then "Template"
-    else                                ""
+    when 'clone_to_template' then "Template"
+    else                          ""
     end
   end
 end
