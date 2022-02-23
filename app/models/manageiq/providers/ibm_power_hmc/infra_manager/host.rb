d90c9c63ca10906a553c23e1a74e2e9abbd50f5b
Vincent Febvre <vfebvre@us.ibm.com>
2022-02-23 14:37:41 +0100

    Add performance metrics collection for hosts

--- app/models/manageiq/providers/ibm_power_hmc/infra_manager/host.rb
+++ app/models/manageiq/providers/ibm_power_hmc/infra_manager/host.rb
@@ -1,6 +1,84 @@
 class ManageIQ::Providers::IbmPowerHmc::InfraManager::Host < ::Host
-  def capture_metrics(_counters, _start_time = nil, _end_time = nil)
-    # TO DO: capture host metrics
-    {}
+  def capture_metrics(counters, start_time = nil, end_time = nil)
+    metrics = {}
+    ext_management_system.with_provider_connection do |connection|
+      samples = connection.managed_system_metrics(
+        :sys_uuid  => ems_ref,
+        :start_ts  => start_time,
+        :end_ts    => end_time
+      )
+      samples.first["systemUtil"]["utilSamples"].each do |s|
+        ts = Time.xmlschema(s["sampleInfo"]["timeStamp"])
+        metrics[ts] = {}
+        counters.each_key do |key|
+          metrics[ts][key] =
+            case key
+            when "cpu_usage_rate_average"
+              cpu_usage_rate_average(s["serverUtil"]["processor"])
+            when "disk_usage_rate_average"
+              s["viosUtil"].map do |vios|
+                disk_usage_rate_average_vios(vios["storage"])
+              end.sum
+            when "mem_usage_absolute_average"
+              mem_usage_absolute_average(s["serverUtil"]["memory"])
+            when "net_usage_rate_average"
+              net_usage_rate_average_server(s["serverUtil"]["network"]) +
+                s["viosUtil"].map do |vios|
+                  net_usage_rate_average_vios(vios["network"])
+                end.sum
+            end
+        end
+      end
+    rescue IbmPowerHmc::Connection::HttpError => e
+      $ibm_power_hmc_log.error("error getting performance samples for host #{ems_ref}: #{e}")
+      unless e.msg.eql?("403 Forbidden") # TO DO - Capture should be disabled at Host level if PCM is not enabled
+        raise
+      end
+    end
+    metrics
+  end
+
+  private
+
+  SAMPLE_DURATION = 30.0 # seconds
+
+  def cpu_usage_rate_average(sample)
+    100.0 * sample["utilizedProcUnits"].sum / sample["configurableProcUnits"].sum
+  end
+
+  def disk_usage_rate_average_vios(sample)
+    usage = 0.0
+    sample.each do |_adapter_type, adapters|
+      adapters.select { |a| a.is_a?(Hash) }.each do |adapter|
+        usage += adapter["transmittedBytes"].sum
+      end
+    end
+    usage / SAMPLE_DURATION / 1.0.kilobyte
+  end
+
+  def mem_usage_absolute_average(sample)
+    100.0 * sample["assignedMemToLpars"].sum / sample["configurableMem"].sum
+  end
+
+  def net_usage_rate_average_server(sample)
+    usage = 0.0
+    sample.each do |_adapter_type, adapters|
+      adapters.each do |adapter|
+        adapter["physicalPorts"].each do |phys_port|
+          usage += phys_port["transferredBytes"].sum
+        end
+      end
+    end
+    usage / SAMPLE_DURATION / 1.0.kilobyte
+  end
+
+  def net_usage_rate_average_vios(sample)
+    usage = 0.0
+    sample.each do |_adapter_type, adapters|
+      adapters.select { |a| a.is_a?(Hash) }.each do |adapter|
+        usage += adapter["transferredBytes"].sum
+      end
+    end
+    usage / SAMPLE_DURATION / 1.0.kilobyte
   end
 end
