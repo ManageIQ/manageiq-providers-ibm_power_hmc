5424816ff1b1155bc733d4520805d77196392fe7
Vincent Febvre <vfebvre@us.ibm.com>
2022-04-13 13:50:19 +0200

    Refactoring factories

--- spec/factories/ext_management_system.rb
+++ spec/factories/ext_management_system.rb
@@ -3,4 +3,23 @@ FactoryBot.define do
           :aliases => ["manageiq/providers/ibm_power_hmc/infra_manager"],
           :class   => "ManageIQ::Providers::IbmPowerHmc::InfraManager",
           :parent  => :ems_infra
+
+  factory :ems_ibm_power_hmc_infra_with_authentication, :parent => :ems_ibm_power_hmc_infra do
+    username = Rails.application.secrets.ibm_power_hmc[:username]
+    password = Rails.application.secrets.ibm_power_hmc[:password]
+    hostname = Rails.application.secrets.ibm_power_hmc[:hostname]
+
+    zone do
+      _guid, _server, zone = EvmSpecHelper.create_guid_miq_server_zone
+      zone
+    end
+
+    endpoints do
+      [FactoryBot.create(:endpoint, :role => "default", :hostname => hostname, :port => 12_443)]
+    end
+
+    after(:create) do |ems|
+      ems.authentications << FactoryBot.create(:authentication, :userid => username, :password => password)
+    end
+  end
 end
