d6f336b0eae78ac7bd21d50f6a1addd6cb0250e6
Vincent Febvre <vfebvre@us.ibm.com>
2022-06-23 14:34:52 +0200

    Make template workflow work with hard-coded target host

--- app/models/manageiq/providers/ibm_power_hmc/infra_manager/provision/cloning.rb
+++ app/models/manageiq/providers/ibm_power_hmc/infra_manager/provision/cloning.rb
@@ -27,7 +27,7 @@ module ManageIQ::Providers::IbmPowerHmc::InfraManager::Provision::Cloning
   def prepare_for_clone_task
     $ibm_power_hmc_log.info("#{self.class}##{__method__}")
     if source.template?
-      {:name => dest_name, :target_sys_uuid => dest_host.ems_ref}
+      {:name => dest_name, :target_sys_uuid => "d47a585d-eaa8-3a54-b4dc-93346276ea37"}
     else
       {:name => dest_name}
     end
@@ -35,7 +35,7 @@ module ManageIQ::Providers::IbmPowerHmc::InfraManager::Provision::Cloning
 
   def log_clone_options(_clone_options)
     if source.template?
-      $ibm_power_hmc_log.info("Deploying template [#{source.name}] to LPAR [#{dest_name}] on host [#{dest_host.name}]")
+      $ibm_power_hmc_log.info("Deploying template [#{source.name}] to LPAR [#{dest_name}] on host [#{dest_host}]")
     else
       $ibm_power_hmc_log.info("Capturing LPAR [#{source.name}] to template [#{dest_name}]")
     end
@@ -44,7 +44,7 @@ module ManageIQ::Providers::IbmPowerHmc::InfraManager::Provision::Cloning
   def start_clone(clone_options)
     $ibm_power_hmc_log.info("start_clone")
     if source.template?
-      source.provision_lpar(clone_options[:name])
+      source.provision_lpar(clone_options)
     else
       source.make_template(clone_options)
     end
--- app/models/manageiq/providers/ibm_power_hmc/infra_manager/template.rb
+++ app/models/manageiq/providers/ibm_power_hmc/infra_manager/template.rb
@@ -2,21 +2,26 @@ class ManageIQ::Providers::IbmPowerHmc::InfraManager::Template < ManageIQ::Provi
   supports :provisioning
 
   def provision_lpar(options)
-    $ibm_power_hmc_log.info("#{self.class}##{__method__} template_name #{name} lpar name #{lpar_name}")
+    $ibm_power_hmc_log.info("#{self.class}##{__method__} template_name #{name} lpar name #{options[:name]}")
     ext_management_system.with_provider_connection do |connection|
+
       check_job = connection.template_check(ems_ref, options[:target_sys_uuid])
       draft_uuid = check_job.results["TEMPLATE_UUID"]
-
-      connection.template_modify(draft_uuid, {:lpar_name => options[:name]})
+      $ibm_power_hmc_log.info("STEP1: #{name}->#{options[:name]}/#{options[:target_sys_uuid]} Checked #{draft_uuid} (#{check_job.inspect})")
 
       transform_job = connection.template_transform(draft_uuid, options[:target_sys_uuid])
       # transform_job.last_status == "COMPLETED_OK"
+      $ibm_power_hmc_log.info("STEP2: #{name}->#{options[:name]}/#{options[:target_sys_uuid]} Transformed #{transform_job.last_status} (#{transform_job.inspect})")
 
-      deploy_job = connection.template_deploy_lpar(ems_ref, name, lpar_name)
+      connection.template_modify(draft_uuid, {:lpar_name => options[:name]})
+      connection.template_modify(draft_uuid, {:lpar_id => nil})
+
+      deploy_job = connection.template_deploy(draft_uuid, options[:target_sys_uuid])
       # @results={"DeployBasePartition"=>"COMPLETED_OK", "PartitionUuid"=>"642B5E86-7C43-4B21-9C26-DADA8B461A9A", "DeployingNetwork"=>"COMPLETED_OK"}
+      $ibm_power_hmc_log.info("STEP3: #{name}->#{options[:name]}/#{options[:target_sys_uuid]} Deployed #{deploy_job.last_status} [#{deploy_job.results["PartitionUuid"]}] (#{deploy_job.inspect})")
       deploy_job.results["PartitionUuid"]
     rescue IbmPowerHmc::Connection::HttpError => e
-      $ibm_power_hmc_log.error("error creating LPAR #{lpar_name} from template #{name}: #{e}")
+      $ibm_power_hmc_log.error("error creating LPAR #{options[:name]} from template #{name}: #{e}")
       raise
     end
   end
