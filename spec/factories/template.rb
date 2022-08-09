d9cf62b44fbe7d4cb193fa249bd37e22c5669df4
Vincent Febvre <vfebvre@us.ibm.com>
2022-08-09 15:27:50 +0200

    Fix VM/Template request workflow
    Signed-off-by: Vincent Febvre <vfebvre@us.ibm.com>

--- spec/factories/template.rb
+++ spec/factories/template.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :ibm_power_hmc_template,
-          :aliases => ["manageiq/providers/infra_manager/template"],
-          :class   => "ManageIQ::Providers::InfraManager::Template",
+          :aliases => ["manageiq/providers/ibm_power_hmc/infra_manager/template"],
+          :class   => "ManageIQ::Providers::IbmPowerHmc::InfraManager::Template",
           :parent  => :template_infra
 end
