006d3980cc3b5bb4ed5d311fc1a60cca7d7faa6b
Damien Bergamini <dbergami@us.ibm.com>
2022-10-03 08:36:14 +0200

    retrieve cluster first as we're using cluster_uuid as ems_ref for storage
    Signed-off-by: Damien Bergamini <dbergami@us.ibm.com>

--- app/models/manageiq/providers/ibm_power_hmc/inventory/collector/target_collection.rb
+++ app/models/manageiq/providers/ibm_power_hmc/inventory/collector/target_collection.rb
@@ -6,50 +6,56 @@ class ManageIQ::Providers::IbmPowerHmc::Inventory::Collector::TargetCollection <
   end
 
   def cecs
-    @cecs ||=
-      references(:hosts).map do |ems_ref|
-        connection.managed_system(ems_ref)
-      rescue IbmPowerHmc::Connection::HttpError => e
-        $ibm_power_hmc_log.error("error querying managed system #{ems_ref}: #{e}") unless e.status == 404
-        nil
-      end.compact
+    @cecs ||= references(:hosts).map do |ems_ref|
+      connection.managed_system(ems_ref)
+    rescue IbmPowerHmc::Connection::HttpError => e
+      $ibm_power_hmc_log.error("error querying managed system #{ems_ref}: #{e}") unless e.status == 404
+      nil
+    end.compact
   end
 
   def lpars
-    @lpars ||=
-      references(:vms).map do |ems_ref|
-        connection.lpar(ems_ref)
-      rescue IbmPowerHmc::Connection::HttpError => e
-        $ibm_power_hmc_log.error("error querying lpar #{ems_ref}: #{e}") unless e.status == 404
-        nil
-      end.compact
+    @lpars ||= references(:vms).map do |ems_ref|
+      connection.lpar(ems_ref)
+    rescue IbmPowerHmc::Connection::HttpError => e
+      $ibm_power_hmc_log.error("error querying lpar #{ems_ref}: #{e}") unless e.status == 404
+      nil
+    end.compact
   end
 
   def vioses
-    @vioses ||=
-      references(:vms).map do |ems_ref|
-        connection.vios(ems_ref)
-      rescue IbmPowerHmc::Connection::HttpError => e
-        $ibm_power_hmc_log.error("error querying vios #{ems_ref}: #{e}") unless e.status == 404
-        nil
-      end.compact
+    @vioses ||= references(:vms).map do |ems_ref|
+      connection.vios(ems_ref)
+    rescue IbmPowerHmc::Connection::HttpError => e
+      $ibm_power_hmc_log.error("error querying vios #{ems_ref}: #{e}") unless e.status == 404
+      nil
+    end.compact
   end
 
   def templates
-    @templates ||=
-      references(:miq_templates).map do |ems_ref|
-        connection.template(ems_ref)
-      rescue IbmPowerHmc::Connection::HttpError => e
-        $ibm_power_hmc_log.error("template query failed for #{ems_ref}: #{e}") unless e.status == 404
-        nil
-      end.compact
+    @templates ||= references(:miq_templates).map do |ems_ref|
+      connection.template(ems_ref)
+    rescue IbmPowerHmc::Connection::HttpError => e
+      $ibm_power_hmc_log.error("error querying template #{ems_ref}: #{e}") unless e.status == 404
+      nil
+    end.compact
   end
 
-  def ssps
-    @ssps ||= begin
-      references(:storage).empty? ? [] : connection.ssps
+  def clusters
+    @clusters ||= references(:storages).map do |ems_ref|
+      connection.clusters(ems_ref)
     rescue IbmPowerHmc::Connection::HttpError => e
-      $ibm_power_hmc_log.error("error querying ssps: #{e}") unless e.status == 404
+      $ibm_power_hmc_log.error("error querying cluster #{ems_ref}: #{e}") unless e.status == 404
+      nil
+    end.compact
+  end
+  private :clusters
+
+  def ssps
+    @ssps ||= clusters.map do |cluster|
+      connection.ssp(cluster.ssp_uuid)
+    rescue IbmPowerHmc::Connection::HttpError => e
+      $ibm_power_hmc_log.error("error querying ssp: #{e}") unless e.status == 404
       []
     end.compact
   end
@@ -57,18 +63,12 @@ class ManageIQ::Providers::IbmPowerHmc::Inventory::Collector::TargetCollection <
   private
 
   def parse_targets!
-    $ibm_power_hmc_log.info("#{self.class}##{__method__}")
-
     target.targets.each do |target|
       case target
       when Host
         add_target!(:hosts, target.ems_ref)
       when ManageIQ::Providers::IbmPowerHmc::InfraManager::Lpar, ManageIQ::Providers::IbmPowerHmc::InfraManager::Vios
         add_target!(:vms, target.ems_ref)
-      when HostSwitch
-        add_target!(:host_virtual_switches, target.ems_ref)
-      when Lan
-        add_target!(:lans, target.ems_ref)
       when ManageIQ::Providers::IbmPowerHmc::InfraManager::Template
         add_target!(:miq_templates, target.ems_ref)
       when ManageIQ::Providers::IbmPowerHmc::InfraManager::Storage
