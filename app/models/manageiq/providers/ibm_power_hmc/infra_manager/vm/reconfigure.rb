67afbcfac25e49a9ce4a1287d4acfebec1a83261
Damien Bergamini <dbergami@us.ibm.com>
2022-11-14 16:02:05 +0100

    split build_config_spec to satisfy codeclimate
    Signed-off-by: Damien Bergamini <dbergami@us.ibm.com>

--- app/models/manageiq/providers/ibm_power_hmc/infra_manager/vm/reconfigure.rb
+++ app/models/manageiq/providers/ibm_power_hmc/infra_manager/vm/reconfigure.rb
@@ -22,60 +22,64 @@ module ManageIQ::Providers::IbmPowerHmc::InfraManager::Vm::Reconfigure
   def build_config_spec(options)
     $ibm_power_hmc_log.debug("building spec for #{options}")
 
-    spec = {}
-
     lpar = ext_management_system.with_provider_connection { |connection| provider_object(connection) }
 
     # Dynamic Reconfiguration requires RMC to be active.
     raise MiqException::MiqVmError, "RMC is not active on target" if lpar.state == "running" && lpar.rmc_state != "active"
 
-    if options.key?(:vm_memory)
-      desired_memory = options[:vm_memory].to_i
+    # HMC does not allow changing the VSWITCH or the VLAN of a client network adapter.
+    # It could be done by deleting and recreating the adapter with the same MAC and options.
+    raise MiqException::MiqVmError, "Cannot edit existing network adapter" if options.key?(:network_adapter_edit)
 
-      raise MiqException::MiqVmError, "Memory cannot be lower than #{lpar.min_memory} MB"   if desired_memory < lpar.min_memory.to_i
-      raise MiqException::MiqVmError, "Memory cannot be greater than #{lpar.max_memory} MB" if desired_memory > lpar.max_memory.to_i
-      spec[:desired_memory] = desired_memory
-    end
-
-    if options.key?(:number_of_cpus)
-      desired_vprocs = options[:number_of_cpus].to_i
-
-      raise MiqException::MiqVmError, "Virtual processor count cannot be lower than #{lpar.minimum_vprocs}"   if desired_vprocs < lpar.minimum_vprocs.to_i
-      raise MiqException::MiqVmError, "Virtual processor count cannot be greater than #{lpar.maximum_vprocs}" if desired_vprocs > lpar.maximum_vprocs.to_i
-      spec[:desired_vprocs] = desired_vprocs
-    end
-
-    if options.key?(:network_adapter_add)
-      spec[:netadap_create] = options[:network_adapter_add].map do |adapt|
-        # Retrieve LAN attributes from DB.
-        switch_ids = HostSwitch.where(:host_id => host.id).pluck(:switch_id)
-        lan = Lan.find_by(:name => adapt[:network], :switch_id => switch_ids)
-        raise MiqException::MiqVmError, "Network [#{adapt[:network]}] is not available on target" if lan.nil?
-
-        {
-          :sys_uuid     => host.ems_ref,
-          :vswitch_uuid => lan.switch.uid_ems,
-          :attrs        => {:vlan_id => lan.tag}
-        }
-      end
-    end
-
-    if options.key?(:network_adapter_edit)
-      # HMC does not allow changing the VSWITCH or the VLAN of a client network adapter.
-      # It would require to delete and recreate the adapter with the same MAC.
-      raise MiqException::MiqVmError, "Cannot edit existing network adapter"
-    end
-
-    if options.key?(:network_adapter_remove)
-      spec[:netadap_delete] = options[:network_adapter_remove].map do |adapt|
-        nic = nics.find_by(:address => adapt[:network][:mac])
-        raise MiqException::MiqVmError, "Network adapter [#{adapt[:network][:mac]}] is not available on target" if nic.nil?
-
-        nic.uid_ems
-      end
-    end
+    spec = {}
+    build_memory_config_spec(lpar, spec, options) if options.key?(:vm_memory)
+    build_vproc_config_spec(lpar, spec, options) if options.key?(:number_of_cpus)
+    build_netadap_create_config_spec(spec, options) if options.key?(:network_adapter_add)
+    build_netadap_delete_config_spec(spec, options) if options.key?(:network_adapter_remove)
 
     spec
+  end
+
+  def build_memory_config_spec(lpar, spec, options)
+    desired_memory = options[:vm_memory].to_i
+
+    raise MiqException::MiqVmError, "Memory cannot be lower than #{lpar.min_memory} MB"   if desired_memory < lpar.min_memory.to_i
+    raise MiqException::MiqVmError, "Memory cannot be greater than #{lpar.max_memory} MB" if desired_memory > lpar.max_memory.to_i
+
+    spec[:desired_memory] = desired_memory
+  end
+
+  def build_vproc_config_spec(lpar, spec, options)
+    desired_vprocs = options[:number_of_cpus].to_i
+
+    raise MiqException::MiqVmError, "Virtual processor count cannot be lower than #{lpar.minimum_vprocs}"   if desired_vprocs < lpar.minimum_vprocs.to_i
+    raise MiqException::MiqVmError, "Virtual processor count cannot be greater than #{lpar.maximum_vprocs}" if desired_vprocs > lpar.maximum_vprocs.to_i
+
+    spec[:desired_vprocs] = desired_vprocs
+  end
+
+  def build_netadap_create_config_spec(spec, options)
+    spec[:netadap_create] = options[:network_adapter_add].map do |adapt|
+      # Retrieve LAN attributes from DB.
+      switch_ids = HostSwitch.where(:host_id => host.id).pluck(:switch_id)
+      lan = Lan.find_by(:name => adapt[:network], :switch_id => switch_ids)
+      raise MiqException::MiqVmError, "Network [#{adapt[:network]}] is not available on target" if lan.nil?
+
+      {
+        :sys_uuid     => host.ems_ref,
+        :vswitch_uuid => lan.switch.uid_ems,
+        :attrs        => {:vlan_id => lan.tag}
+      }
+    end
+  end
+
+  def build_netadap_delete_config_spec(spec, options)
+    spec[:netadap_delete] = options[:network_adapter_remove].map do |adapt|
+      nic = nics.find_by(:address => adapt[:network][:mac])
+      raise MiqException::MiqVmError, "Network adapter [#{adapt[:network][:mac]}] is not available on target" if nic.nil?
+
+      nic.uid_ems
+    end
   end
 
   def raw_reconfigure(spec)
