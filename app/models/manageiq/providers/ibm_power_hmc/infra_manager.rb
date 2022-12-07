e9b56a8d0475c0428b62fa1cf64c13e5f16844d7
Damien Bergamini <dbergami@us.ibm.com>
2022-12-07 09:24:37 +0100

    iso_images through media_repositories
    Signed-off-by: Damien Bergamini <dbergami@us.ibm.com>

--- app/models/manageiq/providers/ibm_power_hmc/infra_manager.rb
+++ app/models/manageiq/providers/ibm_power_hmc/infra_manager.rb
@@ -26,7 +26,7 @@ class ManageIQ::Providers::IbmPowerHmc::InfraManager < ManageIQ::Providers::Infr
 
   has_many :hosts_advanced_settings, :through => :hosts, :source => :advanced_settings
   has_many :media_repositories, :foreign_key => :ems_id, :dependent => :destroy, :inverse_of => :ext_management_system
-  has_many :iso_images, :through => :storages
+  has_many :iso_images, :through => :media_repositories
 
   def self.params_for_create
     {
